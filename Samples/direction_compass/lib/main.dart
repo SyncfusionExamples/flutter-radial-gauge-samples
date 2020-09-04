dfb8127a65eda5b56c00c38d653f7af44af14efc
deviaruna.murugan <deviaruna.murugan@syncfusion.com>
2020-09-04 12:19:21 +0530

    FLUT-2204-[feature][flutter]: Property name changes in direction_compass

--- Samples/direction_compass/lib/main.dart
+++ Samples/direction_compass/lib/main.dart
@@ -45,79 +45,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: SfRadialGauge(
-          axes: <RadialAxis>[
-            RadialAxis(
-              startAngle: 270,
-              endAngle: 270,
-              radiusFactor: 0.9,
-              minimum: 0,
-              maximum: 80,
-              axisLineStyle:
-              AxisLineStyle(thicknessUnit: GaugeSizeUnit.factor, thickness: 0.1),
-              interval: 10,
-              needsRotateLabels: true,
-              axisLabelStyle: GaugeTextStyle(fontSize: 12),
-              minorTicksPerInterval: 0,
-              majorTickStyle: MajorTickStyle(
-                  thickness: 1.5, lengthUnit: GaugeSizeUnit.factor, length: 0.07),
-              showLabels: true,
-              onLabelCreated: labelCreated,
-              pointers: <GaugePointer>[
-                NeedlePointer(
-                    value: 70,
-                    lengthUnit: GaugeSizeUnit.factor,
-                    needleLength: 0.55,
-                    needleEndWidth: 18,
-                    gradient: const LinearGradient(colors: <Color>[
-                      Color(0xFFFF6B78),
-                      Color(0xFFFF6B78),
-                      Color(0xFFE20A22),
-                      Color(0xFFE20A22)
-                    ], stops: <double>[
-                      0,
-                      0.5,
-                      0.5,
-                      1
-                    ]),
-                    needleColor: const Color(0xFFF67280),
-                    knobStyle: KnobStyle(
-                        knobRadius: 0.1,
-                        sizeUnit: GaugeSizeUnit.factor,
-                        color: Colors.white)),
-                NeedlePointer(
-                    gradient: const LinearGradient(colors: <Color>[
-                      Color(0xFFE3DFDF),
-                      Color(0xFFE3DFDF),
-                      Color(0xFF7A7A7A),
-                      Color(0xFF7A7A7A)
-                    ], stops: <double>[
-                      0,
-                      0.5,
-                      0.5,
-                      1
-                    ]),
-                    value: 30,
-                    needleLength: 0.55,
-                    lengthUnit: GaugeSizeUnit.factor,
-                    needleColor:  const Color(0xFFFCACACA),
-                    needleEndWidth: 18,
-                    knobStyle: KnobStyle(
-                        knobRadius: 0.1,
-                        sizeUnit: GaugeSizeUnit.factor,
-                        color: Colors.white))
-              ],
-            )
-          ],
-        )
-      ),
-
+          child: SfRadialGauge(
+        axes: <RadialAxis>[
+          RadialAxis(
+            startAngle: 270,
+            endAngle: 270,
+            radiusFactor: 0.9,
+            minimum: 0,
+            maximum: 80,
+            axisLineStyle: AxisLineStyle(
+                thicknessUnit: GaugeSizeUnit.factor, thickness: 0.1),
+            interval: 10,
+            canRotateLabels: true,
+            axisLabelStyle: GaugeTextStyle(fontSize: 12),
+            minorTicksPerInterval: 0,
+            majorTickStyle: MajorTickStyle(
+                thickness: 1.5, lengthUnit: GaugeSizeUnit.factor, length: 0.07),
+            showLabels: true,
+            onLabelCreated: labelCreated,
+            pointers: <GaugePointer>[
+              NeedlePointer(
+                  value: 70,
+                  lengthUnit: GaugeSizeUnit.factor,
+                  needleLength: 0.55,
+                  needleEndWidth: 18,
+                  gradient: const LinearGradient(colors: <Color>[
+                    Color(0xFFFF6B78),
+                    Color(0xFFFF6B78),
+                    Color(0xFFE20A22),
+                    Color(0xFFE20A22)
+                  ], stops: <double>[
+                    0,
+                    0.5,
+                    0.5,
+                    1
+                  ]),
+                  needleColor: const Color(0xFFF67280),
+                  knobStyle: KnobStyle(
+                      knobRadius: 0.1,
+                      sizeUnit: GaugeSizeUnit.factor,
+                      color: Colors.white)),
+              NeedlePointer(
+                  gradient: const LinearGradient(colors: <Color>[
+                    Color(0xFFE3DFDF),
+                    Color(0xFFE3DFDF),
+                    Color(0xFF7A7A7A),
+                    Color(0xFF7A7A7A)
+                  ], stops: <double>[
+                    0,
+                    0.5,
+                    0.5,
+                    1
+                  ]),
+                  value: 30,
+                  needleLength: 0.55,
+                  lengthUnit: GaugeSizeUnit.factor,
+                  needleColor: const Color(0xFFFCACACA),
+                  needleEndWidth: 18,
+                  knobStyle: KnobStyle(
+                      knobRadius: 0.1,
+                      sizeUnit: GaugeSizeUnit.factor,
+                      color: Colors.white))
+            ],
+          )
+        ],
+      )),
     );
   }
 
